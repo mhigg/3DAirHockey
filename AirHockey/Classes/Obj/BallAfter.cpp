0a7dbd7c5a6f30a4bb5e871d2125f00b6e9b80e7
oka <oka@gmail.com>
2020-01-23 13:51:14 +0900

    画像差し替え

--- AirHockey/Classes/Obj/BallAfter.cpp
+++ AirHockey/Classes/Obj/BallAfter.cpp
@@ -26,9 +26,9 @@ void BallAfter::Update(const cocos2d::Vec3 & lPos)
 		/// 更新した座標を使って、残像の位置とサイズを更新する
 		for (int i = 0; i < _images.size(); ++i)
 		{
-			/// 位置調整のために、画面サイズの半分を減算している(直値なので、修正を必ず行う) ◆
-			_images[i]->setPosition(lpPointWithDepth.SetWorldPosition(_points[i]) - cocos2d::Vec2(1024 / 2, 576 / 2));
-			_images[i]->setScale(lpPointWithDepth.GetScale(_points[i].z));
+			///// 位置調整のために、画面サイズの半分を減算している(直値なので、修正を必ず行う) ◆
+			//_images[i]->setPosition(lpPointWithDepth.SetWorldPosition(_points[i]) - cocos2d::Vec2(1024 / 2, 576 / 2));
+			//_images[i]->setScale(lpPointWithDepth.GetScale(_points[i].z));
 		}
 	}
 	// 時間の更新
@@ -44,14 +44,14 @@ void BallAfter::Init()
 		/// 画像サイズの倍率を計算している
 		rate	= (float)(_images.size() - i) / (_images.size());
 
-		/// 画像の取得
-		_images[i]	= Sprite::create("image/ball/ball_13.png");
+		///// 画像の取得
+		//_images[i]	= Sprite::create("image/ball/ball_13.png");
 
-		/// 画像サイズの設定
-		_images[i]->setContentSize(_images[i]->getContentSize()/* * rate*/);
+		///// 画像サイズの設定
+		//_images[i]->setContentSize(_images[i]->getContentSize()/* * rate*/);
 
-		/// 画像透明度の設定
-		_images[i]->setOpacity(140 * rate);
+		///// 画像透明度の設定
+		//_images[i]->setOpacity(140 * rate);
 		
 		/// 初期座標の設定
 		setPosition(lpPointWithDepth.SetWorldPosition(_localPos));
@@ -59,7 +59,7 @@ void BallAfter::Init()
 		/// 画像スケールの設定
 		setScale(lpPointWithDepth.GetScale(_localPos.z));
 
-		this->addChild(_images[i]);
+		/*this->addChild(_images[i]);*/
 	}
 }
 
